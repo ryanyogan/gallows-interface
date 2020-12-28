5bc473ff20ee9a3ff3d7ffd142befe9b1054b389
Ryan Yogan <ryanyogan@gmail.com>
2020-12-28 13:52:57 -0600

    minor cleanup, added Hangman Server, basic resourece

--- lib/gallows_web/router.ex
+++ lib/gallows_web/router.ex
@@ -13,24 +13,12 @@ defmodule GallowsWeb.Router do
     plug :accepts, ["json"]
   end
 
-  scope "/", GallowsWeb do
+  scope "/hangman", GallowsWeb do
     pipe_through :browser
 
-    get "/", PageController, :index
+    get "/", HangmanController, :new_game
   end
 
-  # Other scopes may use custom stacks.
-  # scope "/api", GallowsWeb do
-  #   pipe_through :api
-  # end
-
-  # Enables LiveDashboard only for development
-  #
-  # If you want to use the LiveDashboard in production, you should put
-  # it behind authentication and allow only admins to access it.
-  # If your application does not have an admins-only section yet,
-  # you can use Plug.BasicAuth to set up some basic authentication
-  # as long as you are also using SSL (which you should anyway).
   if Mix.env() in [:dev, :test] do
     import Phoenix.LiveDashboard.Router
 
